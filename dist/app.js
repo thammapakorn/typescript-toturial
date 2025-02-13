37bf900fabdc36fa16a6af2339fb20fb0c3599ea
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-13 21:59:23 +0700

    ลองสร้างตัวแปร TypeScript
    เอามาลองใช้

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,8 @@
 "use strict";
+let firstname = "Pream";
+let lastname = "Nilpan";
+function totalName() {
+    return firstname + lastname;
+}
+totalName();
+console.log(`ชื่อ = ${firstname} + ${lastname.toUpperCase()}`);
---
+++ src/app.ts
@@ -0,0 +1,9 @@
+let firstname: string = "Pream";
+let lastname: string = "Nilpan";
+
+function totalName() {
+  return firstname + lastname;
+}
+
+totalName();
+console.log(`ชื่อ = ${firstname} + ${lastname.toUpperCase()}`);
