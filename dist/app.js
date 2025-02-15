4c6ca9283b9a4d53b682e1054dc75311cb5662b3
Thammapakorn <121923286+thammapakorn@users.noreply.github.com>
2025-02-15 13:19:12 +0700

    วิธีการใช้ as และ <> ระบุประเภทของตัวแปร

--- dist/app.js
+++ dist/app.js
@@ -1,14 +1,18 @@
 "use strict";
 // any
 // unknown
-function formatNumber(num) {
-    return num.toFixed(2);
-}
-let amount = 50.1234;
-amount = "55.222";
-if (typeof amount === "number") {
-    console.log(formatNumber(amount));
-}
-else {
-    console.log("ค่าไม่ใช่ number");
-}
+// function formatNumber(num: number) {
+//   return num.toFixed(2);
+// }
+// let amount: unknown = 50.1234;
+// amount = "55.222";
+// if (typeof amount === "number") {
+//   console.log(formatNumber(amount));
+// } else {
+//   console.log("ค่าไม่ใช่ number");
+// }
+// type Assertion
+let username;
+username = "Pream";
+console.log(username.toUpperCase());
+console.log(username.toUpperCase());
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,20 @@
 // any
 
 // unknown
-function formatNumber(num: number) {
-  return num.toFixed(2);
-}
+// function formatNumber(num: number) {
+//   return num.toFixed(2);
+// }
 
-let amount: unknown = 50.1234;
-amount = "55.222";
-if (typeof amount === "number") {
-  console.log(formatNumber(amount));
-}else{
-    console.log("ค่าไม่ใช่ number")
-}
+// let amount: unknown = 50.1234;
+// amount = "55.222";
+// if (typeof amount === "number") {
+//   console.log(formatNumber(amount));
+// } else {
+//   console.log("ค่าไม่ใช่ number");
+// }
+
+// type Assertion
+let username:unknown;
+username="Pream";
+console.log((username as string).toUpperCase())
+console.log((<string>username).toUpperCase())
